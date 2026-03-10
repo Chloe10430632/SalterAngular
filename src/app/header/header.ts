2d77a7e6abddfdf2dee551a9054f7d02477bd053
Chloe10430632 <a21307096@gmail.com>
2026-03-11 00:25:22 +0800

    新增header btnActive邏輯

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,46 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  isForumActive: boolean = true;
+  isTripActive: boolean = false;
+  isHouseActive: boolean = false;
+  isExperienceActive: boolean = false;
+
+  btnForumActive() {
+    this.isForumActive = true;
+    this.isTripActive = false;
+    this.isHouseActive = false;
+    this.isExperienceActive = false;
+  }
+
+  btnTripActive() {
+    this.isForumActive = false;
+    this.isTripActive = true;
+    this.isHouseActive = false;
+    this.isExperienceActive = false;
+  }
+
+  btnHouseActive() {
+    this.isForumActive = false;
+    this.isTripActive = false;
+    this.isHouseActive = true;
+    this.isExperienceActive = false;
+  }
+
+  btnExperienceActive() {
+    this.isForumActive = false;
+    this.isTripActive = false;
+    this.isHouseActive = false;
+    this.isExperienceActive = true;
+  }
 
 }
+
