920927fc72951f2cd3c14e2864d8a695747a05af
Chloe10430632 <a21307096@gmail.com>
2026-03-10 16:32:50 +0800

    新增main Component

---
+++ src/app/forum/index/index.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  imports: [],
+  templateUrl: './index.html',
+  styleUrl: './index.css',
+  encapsulation: ViewEncapsulation.None,
+})
+export class Index {
+
+}
---
+++ src/app/main/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Index } from "../forum/index";
+
+@Component({
+  selector: 'app-main',
+  imports: [Index],
+  templateUrl: './main.html',
+  styleUrl: './main.css',
+})
+export class Main {
+
+}
