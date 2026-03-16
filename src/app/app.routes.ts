d83e5205f57c4fe02df4f8449259d62e4348c1b7
Chloe10430632 <a21307096@gmail.com>
2026-03-16 21:02:29 +0800

    feat:所有看板/熱門看板/追蹤推薦看板路由設定完成，準備打Api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,38 @@
+import { Boards } from './forum/components/boards/boards';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: '', //首頁
-    loadComponent: () => import('./forum/components/index/index').then(m => m.Index)
-  },
-  {
-    path: '**', //404 Not Found
-    loadComponent: () => import('./shared/notfound/notfound').then(m => m.Notfound)
+    loadComponent: () => import('./forum/components/index/index').then(m => m.Index),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./forum/components/posts/posts').then(m => m.Posts)
+      }
+    ]
   },
   //討論版路由
   {
     path: 'forum',
-    // loadComponent: () => import('./forum/component/index/index').then(m => m.Index), 先套用 Main 組件，裡面再套用 Index 組件
+    loadComponent: () => import('./forum/components/index/index').then(m => m.Index), //先套用 Main 組件，裡面再套用 Index 組件
     children: [
       {
         path: '',
-        loadComponent: () => import('./forum/components/index/index').then(m => m.Index)
+        loadComponent: () => import('./forum/components/posts/posts').then(m => m.Posts)
+      },
+      {
+        path: 'posts',
+        loadComponent: () => import('./forum/components/posts/posts').then(m => m.Posts)
+      },
+      {
+        path: 'boards',
+        loadComponent: () => import('./forum/components/boards/boards').then(m => m.Boards)
       },
       {
         path: '**', //導到 NotFound
         loadComponent: () => import('./shared/notfound/notfound').then(m => m.Notfound)
       }
-      // {
-      //   path: 'boards',
-      //   loadComponent: () => import('./forum/component/boards/boards').then(m => m.Boards)
-      // },
       // {
       //   path: 'boards/:id',
       //   loadComponent: () => import('./forum/component/board/board').then(m => m.Board)
@@ -41,7 +48,11 @@ export const routes: Routes = [
         loadComponent: () => import('./experience/components/index/index').then(m => m.Index)
       },
     ]
-  }
+  },
+  {
+    path: '**', //404 Not Found
+    loadComponent: () => import('./shared/notfound/notfound').then(m => m.Notfound)
+  },
 
 
 
