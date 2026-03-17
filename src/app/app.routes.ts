e4b98b4aacfcd891fd2e685f59b2403490dca4f8
ericlin1308-lgtm <eric.lin1308@gmail.com>
2026-03-17 16:29:01 +0800

    feat:測試註冊成功

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { Boards } from './forum/components/boards/boards';
 import { Routes } from '@angular/router';
 import { NavbarLayout } from './shared/layouts/navbar-layout/navbar-layout';
@@ -68,6 +69,30 @@ export const routes: Routes = [
       }
     ]
   },
+  //login路由
+  {
+    path: 'login',
+    component: BlankLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./user/components/login/login').then(m => m.Login),
+      },
+    ]
+  },
+  //註冊
+  {
+    path: 'register',
+    component: NavbarLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./user/components/register/register').then(m => m.Register),
+      },
+    ]
+  },
+
+
   {
     path: '**', //萬用路由404
     loadComponent: () => import('./shared/layouts/with-navbar/with-navbar').then(m => m.WithNavbar),
@@ -77,7 +102,8 @@ export const routes: Routes = [
         loadComponent: () => import('./shared/notfound/notfound').then(m => m.Notfound),
       }
     ]
-  },
+  }
+
 
 
 
