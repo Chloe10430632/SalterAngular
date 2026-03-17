9555ee25c5ba044ca9dd1adbd12f597bd96c75d2
Chloe10430632 <a21307096@gmail.com>
2026-03-17 13:49:11 +0800

    feat: 修改路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -67,7 +67,7 @@ export const routes: Routes = [
     ]
   },
   {
-    path: '**',
+    path: '**', //萬用路由404
     loadComponent: () => import('./shared/layouts/with-navbar/with-navbar').then(m => m.WithNavbar),
     children: [
       {
