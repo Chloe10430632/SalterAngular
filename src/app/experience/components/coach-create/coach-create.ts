f442872e5d0c9075ec94c9130314881296200e44
Chloe10430632 <a21307096@gmail.com>
2026-03-24 11:53:49 +0800

    merge:合併完調整細節，先註解CoachCreate

--- src/app/experience/components/coach-create/coach-create.ts
+++ src/app/experience/components/coach-create/coach-create.ts
@@ -11,15 +11,15 @@ import { SCoachPersonalInformation } from '../../Service/Scoach-personal-informa
 export class CoachCreate implements OnInit {
   constructor(private scinfo: SCoachPersonalInformation) { }
   ngOnInit() {
-    if (!this.scinfo.hasProfile) {
-      // 如果有資料，就去 API 抓取並填入表單
-      this.scinfo.coachData(this.coachId).subscribe((data) => {
-        this.scinfo.patchValue(data); // 將 API 資料填入 DaisyUI 表單
-      });
-    } else {
-      // 沒申請過，保持表單為預設空值
-      this.scinfo.reset();
-    }
+    // if (!this.scinfo.hasProfile) {
+    //   // 如果有資料，就去 API 抓取並填入表單
+    //   this.scinfo.coachData(this.coachId).subscribe((data) => {
+    //     this.scinfo.patchValue(data); // 將 API 資料填入 DaisyUI 表單
+    //   });
+    // } else {
+    //   // 沒申請過，保持表單為預設空值
+    //   this.scinfo.reset();
+    // }
   }
 
   cancel() {
