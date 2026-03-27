0b60bd8d12e036f71a0c8aaa769b7dc380de6916
Chloe10430632 <a21307096@gmail.com>
2026-03-27 15:22:23 +0800

    merge:調整ad-services大小寫問題

--- src/app/forum/services/ads-service.ts
+++ src/app/forum/services/ads-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AdData } from '../interfaces/AdData';
+import { AdData } from '../interfaces/adData';
 import { map, timer, zip } from 'rxjs';
 
 @Injectable({
