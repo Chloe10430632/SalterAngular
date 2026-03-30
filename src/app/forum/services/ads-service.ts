9fedebe7289ec01f952a7f3c666d758116db0800
ericlin1308-lgtm <eric.lin1308@gmail.com>
2026-03-30 22:03:04 +0800

    fix:AdData問題

--- src/app/forum/services/ads-service.ts
+++ src/app/forum/services/ads-service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, timer, zip } from 'rxjs';
 import { environment } from './../../../environments/environment';
-import { AdData } from '../interfaces/adData';
+import { AdData } from '../interfaces/AdData';
 
 @Injectable({
   providedIn: 'root',
