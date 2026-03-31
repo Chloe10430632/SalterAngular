f6af5062cdba3184339aa63ea5d609a7f84c2a11
melody803 <melody803@gmail.com>
2026-03-31 11:52:10 +0800

    檢視pf地區

--- src/app/forum/services/ads-service.ts
+++ src/app/forum/services/ads-service.ts
@@ -2,8 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, timer, zip } from 'rxjs';
 import { environment } from './../../../environments/environment';
-import { AdDetails } from '../interfaces/AdDetails';
-
+import { AdData } from '../interfaces/AdData';
 
 @Injectable({
   providedIn: 'root',
